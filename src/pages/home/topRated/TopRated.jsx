c007e50adbb612d16d7d162da2063756eef7e06a
Sumit Kaushik <139322529+kaushiksum@users.noreply.github.com>
2023-10-19 12:28:51 +0530

    Update TopRated.jsx

--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -3,7 +3,7 @@ import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import { useState } from "react";
 import useFetch from "../../../hooks/useFetch";
-import Carousel from "../../../components/carousel/carousel";
+import Carousel from "../../../components/carousel/Carousel";
 
 const TopRated = () => {
     const [endpoint, setEndpoint] = useState("movie")
