2e0d4d07a010181acf2c20ea828cd59b95271759
Sumit Kaushik <139322529+kaushiksum@users.noreply.github.com>
2023-10-19 12:30:56 +0530

    Update Similar.jsx

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Carousel from "../../../components/carousel/carousel";
+import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Similar = ({ mediaType, id }) => {
